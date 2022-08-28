c3704b5eb76a72284e5c298ae40a08ac94cc083e
Andres Narvaez <andres242002@hotmail.com>
2022-08-28 09:33:55 -0500

    inicio

---
+++ proyecto/src/main/java/enigma/dt/proyecto/ProyectoApplication.java
@@ -0,0 +1,14 @@
+package enigma.dt.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApplication.class, args);
+		System.out.println("o");
+	}
+
+}
